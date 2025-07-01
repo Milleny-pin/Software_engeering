56c466fe5fe516f9ca40e21bf6907d40a9288bd2
Milleny-pin <millenybragh@gmail.com>
2025-07-01 20:58:01 -0300

    Funções iniciais prints e input

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Selecione as opções abaixo:")
+print("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão") 
+
+opcao_selecionada = input("Selecione uma opção: ")
+
+print(f"Você selecionou a opção: {opcao_selecionada}")
