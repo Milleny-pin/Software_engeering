091783e96df687e6dc5b042c5afb9856d709fabb
Milleny-pin <millenybragh@gmail.com>
2025-07-01 21:11:22 -0300

    Adicionado a função subtração

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ print("4 - Divisão")
 opcao_selecionada = input("Selecione uma opção: ")
 
 n1 = input("Digite um número: ")
-n2 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
 
 print(f"Você selecionou a opção: {opcao_selecionada}")
 
 if opcao_selecionada == 1:
     resultado = n1 + n2 
+    print(f"O resultado é {resultado}")
+
+if opcao_selecionada == 2:
+    resultado = n1 - n2
     print(f"O resultado é {resultado}")
